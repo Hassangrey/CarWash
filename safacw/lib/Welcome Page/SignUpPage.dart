21300acd78708ad63affcb84c2be56f4db01e3a5
Hassangrey <103444926+Hassangrey@users.noreply.github.com>
2022-04-17 22:26:47 +0300

    Update SignUpPage.dart

--- safacw/lib/Welcome Page/SignUpPage.dart
+++ safacw/lib/Welcome Page/SignUpPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:safacw/Constants/appBarCustomized.dart';
 
+import '../Details Page/DetailPageBar.dart';
+
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
   static const String id = '/SignUpPage';
@@ -19,49 +21,52 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: kappBarStyle('Sign Up'),
         body: Center(
           child: Form(
             key: _formKey,
-            child: Column(
-              children: [
-                Text("Email"),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                ),
-                Text("Password"),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Validate returns true if the form is valid, or false otherwise.
-                    if (_formKey.currentState!.validate()) {
-                      // If the form is valid, display a snackbar. In the real world,
-                      // you'd often call a server or save the information in a database.
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
-                  },
-                  child: const Text('Submit'),
-                ),
-                ElevatedButton(
-                  child: Text('already have an account?'),
-                  onPressed: () {
-                    Navigator.popAndPushNamed(context, '/login');
-                  },)
-              ],
+            child: SafeArea(
+              child: Column(
+                children: [
+                  DetailPageBar(tital: 'SIGN UP', onPress: () {}
+                  ),
+                  Text("Email"),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  Text("Password"),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+                        // If the form is valid, display a snackbar. In the real world,
+                        // you'd often call a server or save the information in a database.
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                  ElevatedButton(
+                    child: Text('already have an account?'),
+                    onPressed: () {
+                      Navigator.popAndPushNamed(context, '/login');
+                    },)
+                ],
+              ),
             ),
           ),
         ));
