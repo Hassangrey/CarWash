f52459365621385da0207520905cae5a20f3aa97
Hassangrey <103444926+Hassangrey@users.noreply.github.com>
2022-05-07 16:52:10 +0300

    s

--- safacw/lib/screens/cart_page.dart
+++ safacw/lib/screens/cart_page.dart
@@ -12,51 +12,30 @@ class CartPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            DetailPageBar(tital: 'CART', onPress: () {}
+            DetailPageBar(tital: 'CART', onPress: () {}),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+
+                  // The list of items
+                  // width: double.infinity,
+                  height: 500,
+                  child: ListView.builder(
+                    // to build the list
+                    itemCount: item.length,
+                    itemBuilder: (context, i) {
+                      return ListTile(
+                        // The list
+                        title: Text("${item[i]['carSize']}"),
+                        subtitle: Text("${item[i]['cost']}" +
+                            "SR" "     " +
+                            "${item[i]['service']}"),
+                        leading: Image.asset('images/smallcar.png'),
+                        trailing: Icon(Icons.delete_sharp),
+                      );
+                    },
+                  )),
             ),
-            Container(
-              // The list of items
-              // width: double.infinity,
-                height: 500,
-                child: ListView.builder(
-                  // to build the list
-                  itemCount: item.length,
-                  itemBuilder: (context, i) {
-                    return ListTile(
-                      // The list
-                      title: Text("${item[i]['carSize']}"),
-                      subtitle: Text("${item[i]['service']}"),
-                      leading: Icon(Icons.car_rental),
-                      trailing: Text("${item[i]['cost']}" + "SR"),
-                      onTap: () => showDialog(
-                        // the window appears on Tap
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              actions: [
-                                FlatButton(
-                                    onPressed: () {
-                                      ;
-                                    },
-                                    child: Text("Yes")),
-                                FlatButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text("Cancel"))
-                              ],
-                              // title:
-                              //     Text("Alert: ", style: TextStyle(fontSize: 15)),
-                              content: Text(
-                                "Do you want to delete the " +
-                                    "${item[i]['carSize']}",
-                                style: TextStyle(fontSize: 13),
-                              ),
-                            );
-                          }),
-                    );
-                  },
-                )),
             Container(
               alignment: Alignment.bottomLeft,
               margin: EdgeInsets.only(left: 10),
@@ -64,19 +43,34 @@ class CartPage extends StatelessWidget {
                 "The total cost is: " + "${total(item)}" + "SR",
                 style: TextStyle(fontSize: 18),
               ),
+            ),
+            SizedBox(height: 30),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 70,
+                decoration: BoxDecoration(
+                    color: Color(0xFFABE8FF),
+                    borderRadius: BorderRadius.circular(30)),
+                width: double.infinity,
+                child: Center(
+                  child: Text(
+                    'ADD TO CART',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ), // ADD TO CART BUTTON
+              ),
             )
-
           ],
         ),
       ),
     );
   }
 }
-List item = [
-  {"carSize": "Small Car", "service": "Internal", "cost": 20},
-  {"carSize": "Large Car", "service": "External", "cost": 30},
-  {"carSize": "Small Car", "service": "Both", "cost": 40}
-];
+
 // The sum function
 double total(List x) {
   var sum = 0.0;
@@ -85,3 +79,9 @@ double total(List x) {
   }
   return sum;
 }
+
+List item = [
+  {"carSize": "Small Car", "service": "Internal", "cost": 20},
+  {"carSize": "Large Car", "service": "External", "cost": 30},
+  {"carSize": "Small Car", "service": "Both", "cost": 40}
+];
--- safacw/lib/screens/main.dart
+++ safacw/lib/screens/main.dart
@@ -21,7 +21,7 @@ class Safa extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: ItemDetailsPage.id,
+      initialRoute: CartPage.id,
       onGenerateRoute: onGenerateRoute,
     );
   }
