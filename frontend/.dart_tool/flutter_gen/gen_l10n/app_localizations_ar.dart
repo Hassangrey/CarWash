febd838314f6d4b474a3df968ccd8c27351a069e
Hassangrey <99whassan@gmail.com>
2022-09-04 17:58:27 +0300

    k

---
+++ frontend/.dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,15 @@
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get home_page_string => 'الصفحة الرئيسية';
+
+  @override
+  String get username_string => 'اسم المستخدم';
+
+  @override
+  String get language => 'العربية';
+}
--- frontend/ios/Flutter/flutter_export_environment.sh
+++ frontend/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/hassan/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/hassan/Documents/GitHub/CarWash/safacw"
+export "FLUTTER_APPLICATION_PATH=/Users/hassan/Documents/GitHub/CarWash/frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/hassan/Documents/GitHub/CarWash/safacw/lib/main.dart"
+export "FLUTTER_TARGET=/Users/hassan/Documents/GitHub/CarWash/frontend/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -12,4 +12,4 @@ export "DART_OBFUSCATION=false"
 export "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/hassan/Documents/GitHub/CarWash/safacw/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/hassan/Documents/GitHub/CarWash/frontend/.dart_tool/package_config.json"
