3fdc7fa21cb3ddfc1cccae87b6f2730460daf969
Ziyad Alotaibi <ziyad.alotaibe@gmail.com>
2022-09-16 21:11:36 +0300

    add location

--- backend/carwash/models.py
+++ backend/carwash/models.py
@@ -1,3 +1,4 @@
+from re import X
 from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
@@ -19,9 +20,9 @@ class Item(models.Model):
                              on_delete=models.CASCADE)
     name = models.TextField(blank=True)
     description = models.TextField(blank=True)
-    in_price = models.DecimalField(blank=True,decimal_places=5, max_digits=9999, null=True)
-    out_price = models.DecimalField(blank=True,decimal_places=5, max_digits=9999, null=True)
-    both = models.DecimalField(blank=True,decimal_places=5, max_digits=9999, null=True)
+    in_price = models.DecimalField(blank=True,decimal_places=2, max_digits=9999, null=True)
+    out_price = models.DecimalField(blank=True,decimal_places=2, max_digits=9999, null=True)
+    both = models.DecimalField(blank=True,decimal_places=2, max_digits=9999, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
 
 class Order(models.Model):
@@ -32,6 +33,12 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='user_Item',
                              on_delete=models.CASCADE)
+    service_provider = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='service_provider',
+                             on_delete=models.CASCADE)
+    driver = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='driver',
+                             on_delete=models.CASCADE,null=True)
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,  related_name='profile')
@@ -56,6 +63,13 @@ class Profile(models.Model):
     )
     def __str__(self):
         return f'Profile for user {self.user.username}'
+
+class location(models.Model):
+    driver = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='driver_location',
+                             on_delete=models.CASCADE)
+    X = models.DecimalField(blank=True,decimal_places=2, max_digits=9999, null=True)
+    y = models.DecimalField(blank=True,decimal_places=2, max_digits=9999, null=True)
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_profile(sender, instance, created, **kwargs):
